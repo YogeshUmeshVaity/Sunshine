c48048acdf245d516d465a4b5ba7026f5dde590f
Yogesh <yogeshvaity@gmail.com>
2016-01-21 20:32:47 +0530

    Load XML layout for SettingsActivity. Bind preference summery to value.

--- app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/SettingsActivity.java
@@ -20,11 +20,12 @@ public class SettingsActivity extends PreferenceActivity
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.pref_general);
+        // Add 'general' preferences defined in the XML file
+        addPreferencesFromResource(R.xml.pref_general);
 
         // For all preferences, attach an OnPreferenceChangeListener so the UI summary can be
         // updated when the preference changes.
-        // TODO: Add preferences
+        bindPreferenceSummaryToValue(findPreference(getString(R.string.pref_location_key)));
     }
 
     /**
