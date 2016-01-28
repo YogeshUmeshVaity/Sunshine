5d31f83c5aac922bb6c482fcd7e2df0d7daa1b08
Yogesh <yogeshvaity@gmail.com>
2016-01-28 14:00:51 +0530

    Add code for ShareActionProvider. clicking on it not launching the chooser.

--- app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/DetailActivity.java
@@ -16,12 +16,17 @@
 
 package com.example.android.sunshine.app;
 
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.ShareActionProvider;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -70,7 +75,55 @@ public class DetailActivity extends ActionBarActivity {
      */
     public static class DetailFragment extends Fragment {
 
+        private ShareActionProvider shareActionProvider;
+        private String forecastDetails;
+
         public DetailFragment() {
+            setHasOptionsMenu(true);
+        }
+
+        /**
+         * Initialize the contents of the Activity's standard options menu.  You
+         * should place your menu items in to <var>menu</var>.  For this method
+         * to be called, you must have first called {@link #setHasOptionsMenu}.  See
+         * {@link Activity#onCreateOptionsMenu(Menu) Activity.onCreateOptionsMenu}
+         * for more information.
+         */
+        @Override
+        public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+            super.onCreateOptionsMenu(menu, inflater);
+            // Find the menu item with ShareActionProvider
+            MenuItem shareMenuItem = menu.findItem(R.id.action_share);
+
+            // Assign ShareActionProvider object to member variable when the options menu is created.
+            shareActionProvider = (ShareActionProvider) MenuItemCompat.getActionProvider(shareMenuItem);
+
+            // Update the shareActionProvider with the intent.
+            setShareIntent(createShareIntent());
+        }
+
+        /**
+         * Updates the share intent to the shareActionProvider.
+         * @param shareIntent is the intent, the shareActionProvider is to be updated with.
+         */
+        @TargetApi(Build.VERSION_CODES.ICE_CREAM_SANDWICH)
+        private void setShareIntent(Intent shareIntent) {
+            if(shareActionProvider != null) {
+                shareActionProvider.setShareIntent(shareIntent);
+            }
+        }
+
+        /**
+         * Creates intent of weather details to be shared.
+         * @return returns an Intent object with weather data.
+         */
+        private Intent createShareIntent() {
+            Intent shareIntent = new Intent();
+            shareIntent.setAction(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(
+                    Intent.EXTRA_TEXT, forecastDetails + getString(R.string.hashSunshineApp));
+            return shareIntent;
         }
 
         @Override
@@ -80,7 +133,7 @@ public class DetailActivity extends ActionBarActivity {
 
             Intent detailIntent = getActivity().getIntent();
             if(detailIntent != null && detailIntent.hasExtra(Intent.EXTRA_TEXT)) {
-                String forecastDetails = detailIntent.getStringExtra(Intent.EXTRA_TEXT);
+                forecastDetails = detailIntent.getStringExtra(Intent.EXTRA_TEXT);
                 TextView detailTextView = (TextView) rootView.findViewById(R.id.detail_text_view);
                 detailTextView.setText(forecastDetails);
             }
