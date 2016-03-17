25acac721239bc51a7e552fdfa047fb156125cd1
Yogesh <yogeshvaity@gmail.com>
2016-03-17 20:47:59 +0530

    Add files for ContentProvider and it's tests.

--- app/src/main/java/com/example/android/sunshine/app/MainActivity.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivity.java
@@ -9,6 +9,8 @@ import android.view.MenuItem;
 
 public class MainActivity extends ActionBarActivity {
 
+    private static final String LOG_TAG = "MainActivity";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
