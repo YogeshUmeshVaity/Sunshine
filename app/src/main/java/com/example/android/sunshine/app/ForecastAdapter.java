612011ed79351d0d86970d322ec259a0289d3333
Yogesh <yogeshvaity@gmail.com>
2016-04-21 00:35:09 +0530

    Add the item view type for today item.

--- app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
+++ app/src/main/java/com/example/android/sunshine/app/ForecastAdapter.java
@@ -15,23 +15,46 @@ import android.widget.TextView;
  */
 public class ForecastAdapter extends CursorAdapter {
 
+    private static final int VIEW_TYPE_TODAY = 0;
+    private static final int VIEW_TYPE_FUTURE_DAY = 1;
+    private static final int VIEW_TYPE_COUNT = 2;
+
     public ForecastAdapter(Context context, Cursor cursor, int flags) {
         super(context, cursor, flags);
     }
 
-    /*
-        Remember that these views are reused as needed.
-     */
     @Override
-    public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        View view = LayoutInflater.from(context).inflate(R.layout.list_item_forecast, parent, false);
-
-        return view;
+    public int getItemViewType(int position) {
+        // super.getItemViewType(position);
+        return position == 0 ? VIEW_TYPE_TODAY : VIEW_TYPE_FUTURE_DAY;
     }
 
-    /*
-        This is where we fill-in the views with the contents of the cursor.
-     */
+    @Override
+    public int getViewTypeCount() {
+        // super.getViewTypeCount();
+        return VIEW_TYPE_COUNT;
+    }
+
+    // Remember that these views are reused as needed.
+    @Override
+    public View newView(Context context, Cursor cursor, ViewGroup parent) {
+        // Choose the layout type
+        int viewType = getItemViewType(cursor.getPosition());
+        int layoutId = -1;
+        switch (viewType) {
+            case VIEW_TYPE_TODAY: {
+                layoutId = R.layout.list_item_forecast_today;
+                break;
+            }
+            case VIEW_TYPE_FUTURE_DAY: {
+                layoutId = R.layout.list_item_forecast;
+                break;
+            }
+        }
+        return LayoutInflater.from(context).inflate(layoutId, parent, false);
+    }
+
+    //This is where we fill-in the views with the contents of the cursor.
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
         // Read weather icon id from cursor.
